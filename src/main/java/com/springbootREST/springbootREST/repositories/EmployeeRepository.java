5c5a4553af7562edc331a2891f6dbeeda61c6103
carrabbi <thediamondhare@gmail.com>
2026-02-24 22:46:33 +0100

    Spring Boot Data used as dependency

--- src/main/java/com/springbootREST/springbootREST/repositories/EmployeeRepository.java
+++ src/main/java/com/springbootREST/springbootREST/repositories/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.springbootREST.springbootREST.repositories;
 
 import com.springbootREST.springbootREST.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// changing the default name of endpoint
+//@RepositoryRestResource(path="someemployees")
 public interface  EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
